2a19984c699779cb1fba19995101ab263d68d99a
blueconecell <95961799+blueconecell@users.noreply.github.com>
2024-07-30 15:22:28 +0900

    AllSmallFarm - post

--- smallFarm/views.py
+++ smallFarm/views.py
@@ -11,3 +11,14 @@ class AllSmallFarm(APIView):
             return Response(serializer.data)
         except Exception as e:
             return Response({"에러":str(e)})
+    def post(self, request):
+        try:
+            serializer = serializers.SmallFarmSerializer(data = request.data)
+            try:
+                if serializer.is_valid():
+                    farmData = serializer.save()
+                    return Response(serializer.data)
+            except Exception as e:
+                return Response({"test error(valid_error)":str(e)})
+        except Exception as e:
+            return Response({"test error(data error)":str(e)})
